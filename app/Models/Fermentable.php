660ce1563c12c93b9cfec51127536f56cac4e0a7
jaaf <jaaf@localhost.localdomain>
2022-07-11 13:39:56 +0200

    brew

--- app/Models/Fermentable.php
+++ app/Models/Fermentable.php
@@ -9,6 +9,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Fermentable extends Model
 {
     use HasFactory;
+
+      protected $casts = [
+    'brand_id'=>'integer',
+        'quantity'=>'float',
+        'potential'=>'float',
+        'color'=>'float',
+        'diastatic_power'=>'float',
+        'ph'=>'float'
+       
+    ];
     protected $fillable =["name","brand_id","form","type","raw_ingredient","potential","color","diastatic_power",'ph','link','log'];
 
     public function fermentable_brand(){
