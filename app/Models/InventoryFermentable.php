de23847b12a0dfea95271d94da7f3139e757aa1d
jaaf <jaaf@localhost.localdomain>
2022-07-11 13:33:49 +0200

    brew

--- app/Models/InventoryFermentable.php
+++ app/Models/InventoryFermentable.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class InventoryFermentable extends Model
 {
+   protected $casts = [
+        'quantity'=>'float',
+        'price'=>'float',
+        'potential'=>'float',
+        'color'=>'float',
+        'diastatic_power'=>'float',
+        'ph'=>'float'
+       
+    ];
+
     use HasFactory;
     protected $fillable =['user_id','shared_f_id',"quantity","price","currency","name","brand_id","form","type","raw_ingredient","potential","color","diastatic_power","ph","link"];
 }
